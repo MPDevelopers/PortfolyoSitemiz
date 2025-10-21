b8b2b36bbb6ecd517a28895053e99001a0194e19
ahmetakdmrrr <ahmet551977@gmail.com>
2025-10-21 23:41:55 +0300

    deneme 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/PortfolyoSitemiz/', // GitHub Pages için repository adınızı buraya yazın
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 });
